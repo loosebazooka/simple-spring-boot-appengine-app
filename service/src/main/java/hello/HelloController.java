cf166715268739fc2d80449f3ec8d74c96918847
Appu Goundan <appu@google.com>
2018-12-14 14:00:47 -0500

    optional file finder

--- service/src/main/java/hello/HelloController.java
+++ service/src/main/java/hello/HelloController.java
@@ -4,17 +4,24 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
-    public String index() throws IOException {
-        List<String> lines = Files.readAllLines(Paths.get("file.txt"), Charset.defaultCharset());
-        return "Greetings from Spring Boot! " + lines.toString();
+
+  @RequestMapping("/")
+  public String index() throws IOException {
+    Path testFile = Paths.get("file.txt");
+    if (Files.exists(testFile)) {
+      List<String> lines = Files.readAllLines(Paths.get("file.txt"), Charset.defaultCharset());
+      return "Greetings from Spring Boot! " + lines.toString();
     }
-    
+    else {
+      return "Greetings from Spring Boot! Could not find file: " + testFile.toString();
+    }
+  }
+
 }
